68eb74cf32fd2769e931c6facf2bc6e8e2e58775
Jundi <indana6969@gmail.com>
2022-01-10 14:50:00 +0700

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+// components
+import UserResults from "../components/users/UserResults"
+import UserSearch from "../components/users/UserSearch"
+
+function Home() {
+    return (
+        <>
+            <UserSearch />
+            <UserResults />
+        </>
+    )
+}
+
+export default Home
